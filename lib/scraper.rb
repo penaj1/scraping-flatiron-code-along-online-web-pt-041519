a4cbc019c9c81e8b03fb202484fd7fe97d7ea92c
Janice Pena <jipena94@gmail.com>
2019-05-28 22:15:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -32,17 +32,17 @@ class Scraper
   
   def get_courses
     self.get_page.css(".post")
-    end
+  end
     
     
     def make_courses
-  self.get_courses.each do |post|
+      self.get_courses.each do |post|
     course = Course.new
     course.title = post.css("h2").text
     course.schedule = post.css(".date").text
     course.description = post.css("p").text
+    end
   end
-end
   
     def print_courses
     self.make_courses
